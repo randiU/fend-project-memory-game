10a048c87a50ed114b163cd9f4c0aa66068edf43
randiU <33576756+randiU@users.noreply.github.com>
2017-12-14 12:56:53 -0800

    added animation to cards - also started readMe file

--- js/app.js
+++ js/app.js
@@ -277,6 +277,37 @@ function startTimer() {
   });
 }
 
+/*let $el = $(".deck");
+let elHeight = $el.outerHeight();
+let elWidth = $el.outerWidth();
+
+let $wrapper = $(".scale-wrapper");
+
+$wrapper.resizeable({
+  resize: doResize
+});
+
+function doResize(event,ui) {
+  let scale, origin;
+  scale = Math.min(
+    ui.size.width/elWidth,
+    ui.size.height/elHeight
+  );
+
+  $el.css({
+    transform: "translate(-50%, -50%) " + " scale(" + scale + ")"
+  });
+}
+
+let starterData = {
+  size: {
+    width: $wrapper.width(),
+    height: $wrapper.height()
+  }
+}
+
+doResize(null,starterData);*/
+
 
 
 shuffleCards();
